bee3b770566c46dc6fa0f5c059abc751cad08238
Denis Yakovlev <84284965+w3llw3t@users.noreply.github.com>
2021-09-17 15:21:48 +0300

    test
    test

---
+++ test.java
@@ -0,0 +1,40 @@
+package com.company;
+import java.lang.String;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String []a;
+        int n;
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Число строк: ");
+        n = scn.nextInt();
+        a = new String [n];
+        for (int i = 0; i <= n; i++)
+            {
+                if (isDoubloon(scn.nextLine())) {
+                    System.out.println("YES");
+                }
+                else {
+                    System.err.println("NO");
+                }
+                }
+            }
+        public static boolean isDoubloon(final String s) {
+            final String l = s.toLowerCase();
+            for (int i = 0; i < l.length(); i++) {
+                int count = 0;
+                for (int j = 0; j < l.length(); j++) {
+                    if (l.charAt(i) == l.charAt(j)) {
+                        count++;
+                        if (2 < count) {
+                            return false;
+                        }
+                    }
+                }
+                if (1 == count) {
+                    return false;
+                }
+            }
+            return true;
+        }
+}
